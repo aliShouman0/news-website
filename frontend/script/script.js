00408d64efb7544ca77cf4b6fc9ee1bb1e755342
Ali Shouman <alishouman010@gmail.com>
2022-09-23 20:30:12 +0300

    breaking_news api

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,19 +1,25 @@
+// tags needed
 const breakingNews = document.querySelector("#breakingNews");
 const trending = document.querySelector("#news");
 const sport = document.querySelector("#sport");
+const carousel_indicators = document.querySelector("#carousel-indicators");
+const carousel_inner = document.querySelector("#carousel-inner");
+// api 
 const api_sportNews = "http://localhost:3000/backend/sport.php";
-const api_breaking_news = "http://localhost:3000/backend/sport.php";
+const api_breaking_news = "http://localhost:3000/backend/breaking_news.php";
 const api_trending = "http://localhost:3000/backend/trending.php";
 
+//sport News
 $.getJSON(api_sportNews, function (data) {
   data.forEach((news) => {
     sport.innerHTML += `
-     <div class="shadow-lg p-4 mb-5  rounded">
+     <div class="shadow-lg p-4 mb-5 mx-5 rounded">
     ${news.txt}
     </div>
     `;
   });
 });
+
 
 $.getJSON(api_trending, function (data) {
   data.forEach((news) => {
@@ -27,6 +33,39 @@ $.getJSON(api_trending, function (data) {
           </div>
           </div>
            </div>
-          `;
+    `;
+  });
+});
+
+
+
+$.getJSON(api_breaking_news, function (data) {
+  for (let i = 0; i < data.length; i++) {
+    carousel_indicators.innerHTML += `
+    <li data-target="#breakingNews" data-slide-to="${i}"></li> 
+    `;
+  }
+  data.forEach((news, i) => {
+    if (i == 0) {
+      carousel_inner.innerHTML += ` 
+      <div class="carousel-item active">
+      <img class="d-block" src="../backend/${news.img}" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>${news.title}</h5>
+        <p> ${news.txt}</p>
+      </div>
+    </div>
+      `;
+    } else {
+      carousel_inner.innerHTML += ` 
+    <div class="carousel-item  ">
+    <img class="d-block" src="../backend/${news.img}" alt="First slide">
+    <div class="carousel-caption d-none d-md-block">
+      <h5> ${news.title}</h5>
+      <p> ${news.txt}</p>
+    </div>
+  </div>
+    `;
+    }
   });
 });
